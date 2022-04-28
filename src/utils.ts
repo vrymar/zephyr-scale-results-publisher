ad6ee8f5ef756412d58b899e7032afb9706945a3
Vitalii Rymar <vitalii.rymar@ah.nl>
2022-04-28 12:59:01 +0300

    Added utils

---
+++ src/utils.ts
@@ -0,0 +1,39 @@
+import archiver from 'archiver';
+import FormData from 'form-data';
+import fs, { existsSync } from 'fs';
+import path from 'path';
+
+export class Utils {   
+
+  public async createFormDataWithFile(key: string, filePath: string){
+    const form = new FormData();
+    if(existsSync(filePath)){
+      const dataFile = fs.createReadStream(filePath);
+      form.append(key, dataFile);    
+    } else {
+      console.error(`Results file was not found: ${filePath}`)
+    }
+    return form;    
+  }   
+
+  public async zip(zipFilePath: string, sourceFileName: string, sourceDir: string){
+    const sourceFile = path.join(sourceDir, sourceFileName);
+    return new Promise((resolve) => {
+      let output = fs.createWriteStream(zipFilePath);
+      let archive = archiver('zip');
+        
+    output.on('close', async () => {
+      console.log('Archiving is successful');
+      resolve(output);
+    });
+        
+    archive.on('error', function (err: any) {
+      throw err;
+    });
+        
+    archive.pipe(output);
+      archive.append(fs.createReadStream(sourceFile), { name: sourceFileName });
+      archive.finalize();
+    })
+  }
+}
